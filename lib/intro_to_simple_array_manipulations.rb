5ad73f03c60b70fe1760f459172312b51b2c2734
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-08 05:43:21 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,32 @@
+def using_push(array, string)
+  array.push("violet")
+end
+
+def using_unshift(array, string)
+  array.unshift("Staten Island")
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+   array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array, array1)
+  array.concat(array1)
+end
+
+def using_insert(array, element)
+  array.insert(4)
+end
+
